09b3831ec066bfa4a676534ae4de2d0c51d652c6
Zach Keskinen <Zachkeskinen@gmail.com>
2024-01-26 10:24:18 -0700

    snotel figures

--- src/data_acquisition/snotel_ex.py
+++ src/data_acquisition/snotel_ex.py
@@ -1,17 +1,35 @@
+from pathlib import Path
 
 import numpy as np
+import pandas as pd
 import geopandas as gpd
+from shapely import wkb, wkt
+
+import matplotlib.pyplot as plt
 
 from datetime import datetime
+
 from metloom.pointdata import SnotelPointData
+from metloom.variables import SnotelVariables
 
+fig_dir = Path('/bsuhome/zacharykeskinen/uavsar-coherence/figures/snotels')
 
+bounds_fp = Path('/bsuhome/zacharykeskinen/uavsar-coherence/data/uavsar-bounds/snowex-uavsar-bounds-v2.shp')
+bounds = gpd.read_file(bounds_fp)
+_drop_z = lambda geom: wkb.loads(wkb.dumps(geom, output_dimension=2))
+bounds.geometry = bounds.geometry.transform(_drop_z)
 
-gpd.read_file()
+snotels = pd.read_csv('/bsuhome/zacharykeskinen/uavsar-coherence/data/snotel/snotel-list.csv')
+snotels = gpd.GeoDataFrame(snotels, geometry=gpd.points_from_xy(snotels.Longitude, snotels.Latitude), crs='epsg:4326')
 
-snotel_point = SnotelPointData("713:CO:SNTL", "MyStation")
-df = snotel_point.get_daily_data(
-    datetime(2020, 1, 2), datetime(2020, 1, 20),
-    [snotel_point.ALLOWED_VARIABLES.SWE]
-)
-print(df)
+fig, ax= plt.subplots()
+snotels.plot(ax = ax, markersize = 1)
+bounds.plot(ax = ax, color = 'red', zorder = 1e3)
+
+intersect = snotels.sjoin(bounds, how = 'inner')
+intersect.plot(ax = ax, color = 'green', zorder = 1e4)
+print(intersect)
+
+ax.set_xlim(-125, -105)
+ax.set_ylim(33, 48)
+plt.savefig(fig_dir.joinpath('snotel_uavsar.png'))
