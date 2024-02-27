cd76bbd73aced6a3f633c1a26c8a1cb1bd13e304
Zach Keskinen <Zachkeskinen@gmail.com>
2024-02-26 17:09:42 -0700

    generating variogram and coarsening resolution figures

--- src/compare/scaling/variograms.py
+++ src/compare/scaling/variograms.py
@@ -23,7 +23,9 @@ fig_dir = home_dir.joinpath('figures', 'variograms')
 uavsar_dir = Path('/bsuhome/zacharykeskinen/scratch/coherence/uavsar/tiffs/')
 
 # get list of uavsars directories
-uavsars = sorted(list(uavsar_dir.glob('*')))
+uavsars = list(uavsar_dir.glob('*')) # sorted
+import random
+random.shuffle(uavsars)
 # remove tmp dir
 uavsars = [u for u in uavsars if u != 'tmp']
 # get location and flight direction for key and first coherence tiff as value of dictionary
@@ -32,11 +34,11 @@ uavsars = {u.stem: list(u.glob('*.cor.grd.tiff'))[0] for u in uavsars if len(lis
 n = 1000
 fig, ax = plt.subplots()
 for i, (uavsar, fp) in tqdm(enumerate(uavsars.items()), total = len(uavsars)):
-    if i == 1: break
+    if i == 4: break
     img = rxa.open_rasterio(fp).squeeze('band', drop = True)
     img = img.rio.write_crs('EPSG:4326').rio.reproject(dst_crs = img.rio.estimate_utm_crs())
     img = img.where((img > 0) & (img < 1))
-    img = img.sel(x = slice(img.x.mean() - 1000, img.x.mean() + 1000), y = slice(img.y.mean() +1000, img.y.mean() - 1000))
+    img = img.sel(x = slice(img.x.mean() - 2500, img.x.mean() + 2500), y = slice(img.y.mean() +2500, img.y.mean() - 2500))
     img = img.dropna('x', how = 'all').dropna('y', how = 'all')
     coords = list(product(np.random.choice(img.x, 100), np.random.choice(img.y, 100)))
     values = np.array([img.sel(x = x, y = y).values for x, y in coords]).ravel()
@@ -45,15 +47,25 @@ for i, (uavsar, fp) in tqdm(enumerate(uavsars.items()), total = len(uavsars)):
     
     print(len(values))
     print(img.std().values)
+
     fig_sub, ax_sub = plt.subplots()
     img.plot(ax = ax_sub)
     ax_sub.scatter(coords[:, 0], coords[:, 1], marker = 'x', s = 1, color = 'black')
-    fig_sub.savefig(fig_dir.joinpath(f'test_img_{uavsar}.png'))
+    fig_sub.savefig(fig_dir.joinpath('sampling', f'sampling_{uavsar}.png'))
+    plt.close(fig_sub)
+    for (coarsen_x, coarsen_y), res in zip([(1,1), (2,2),(4,4),(6,6), (10,10), (20,20)], ['5m', '10m', '20m', '30m', '50m', '100m']):
+        fig_sub, ax_sub = plt.subplots()
+        img.coarsen(x = coarsen_x, y= coarsen_y, boundary = 'trim').mean().plot(ax = ax_sub)
+        ax_sub.set_title(f'Resolution = {res}')
+        fig_sub.savefig(fig_dir.joinpath('coarsen', f'{uavsar}_coarsen_{res}.png'))
+        plt.close(fig_sub)
 
-    V = skg.Variogram(coords, values, bin_func = 'scott', use_nugget = True, model = 'exponential', ) # maxlag = 20000, n_lags = 100,
+
+    V = skg.Variogram(coords, values, n_lags = 300, bin_func = 'uniform', use_nugget = True, model = 'exponential', maxlag = 1000) # maxlag = 20000, n_lags = 100,
     V.plot(axes = ax, grid = False, show = False, hist = False)
     # V.distance_difference_plot()
 
+ax.axvline(30)
 # [l.set_color("black") for l in ax.get_lines()]
 # [l.set_linestyle('--') for l in ax.get_lines()[1::2]]
 # [l.set_visible(False) for l in ax.get_lines()[::2]]
