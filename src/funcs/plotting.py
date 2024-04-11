efe4b60ca0db9fab930c223f1e88ccb0f7523820
Zach Keskinen <Zachkeskinen@gmail.com>
2024-04-11 13:03:40 -0600

    snow factors figures

--- src/funcs/plotting.py
+++ src/funcs/plotting.py
@@ -48,3 +48,9 @@ def plt_lc(lc, ax):
         ['Water/Wetlands', 'Developed', 'Barren', 'Forests', 'Shrubs/Grass', 'Cultivated', 'Perennial Snow']):
         patches.append(mpatches.Patch(color=color(1000), label=label))
     ax.legend(handles=patches)
+
+def add_text(s, ax, loc):
+    locs = {'upper right': {'xy': (0.99, 0.99), 'va': 'top', 'ha': 'right'}, 'upper left': {'xy': (0.01, 0.99), 'va': 'top', 'ha': 'left'}, 'lower left': {'xy': (0.01, 0.01), 'va': 'bottom', 'ha': 'left'}, 'lower right': {'xy': (0.99, 0.01), 'va': 'bottom', 'ha': 'right'}}
+    assert loc in locs.keys(), f'loc must be in set of : {locs.keys()}'
+    fontdic = locs[loc]
+    ax.text(*fontdic['xy'], s, va = fontdic['va'], ha = fontdic['ha'], transform= ax.transAxes)
